9efb934c833df2f054fb4435ed9f80f14a07ae14
Marc Garcia <garcia.marc.001@gmail.com>
2021-09-18 11:12:59 -0400

    layout of login and register componenets

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+// import { loginUser } from "../api"
+// import { storeToken } from "../auth"
+
+const Login = ({ setIsLoading, setIsLoggedIn }) => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-main-container">
+      <form
+        id="login"
+        // ask about on submit and the try catch block
+        onSubmit={async (event) => {
+          event.preventDefault();
+          setIsLoading(true);
+
+          try {
+            const resuls = await loginUser(userName, password);
+            storeToken(results.data.token);
+            setIsLoggedIn(true);
+
+            setUserName("");
+            setPassword("");
+          } catch (error) {
+            console.log(error);
+          } finally {
+            setIsLoading(false);
+          }
+        }}
+      >
+        <fieldset>
+          <label htmlFor="userName">User Name</label>
+          <input
+            type="text"
+            placeholder="User Name"
+            min="8"
+            id="userName"
+            value={userName}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            placeholder="Password"
+            id="password"
+            value={password}
+          />
+        </fieldset>
+
+        <button>Login</button>
+      </form>
     </div>
   );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  //   import { registerUser } from "../api";
+  // import { storeToken } from "../auth";
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="register-main-container">
+      <form
+        id="register"
+        onSubmit={async (event) => {
+          event.preventDefault();
+          setIsLoading(true);
+
+          try {
+            const results = await registerUser(userName, password);
+            storeToken(results.data.token);
+            setIsLoggedIn(true);
+
+            setUserName("");
+            setPassword("");
+          } catch (err) {
+            console.log(err);
+          } finally {
+            setIsLoading(false);
+          }
+        }}
+      >
+        <fieldset>
+          <label htmlFor="userName">User Name</label>
+          <input
+            type="text"
+            placeholder="User Name"
+            min="8"
+            id="userName"
+            value={userName}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            placeholder="Password"
+            id="password"
+            value={password}
+          />
+        </fieldset>
+
+        <button>Register</button>
+      </form>
     </div>
   );
 };
